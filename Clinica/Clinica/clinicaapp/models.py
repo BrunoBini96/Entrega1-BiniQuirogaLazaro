73f5bb0d35b0cbe15e9750d5a9c39e3e3ef8b88f
Bruno <brunobini96@gmail.com>
2022-11-01 09:23:26 -0300

    Se agregaron los models a la DB

--- Clinica/Clinica/clinicaapp/models.py
+++ Clinica/Clinica/clinicaapp/models.py
@@ -1,23 +1,24 @@
 from django.db import models
-
+from unittest.util import _MAX_LENGTH
 class Turnos(models.Model):
-    doctor = models.CharField(MAX_LENGTH = 20)
-    paciente = models.CharField(MAX_LENGTH = 8)
+    doctor = models.CharField(max_length=20)
+    paciente = models.CharField(max_length=20)
     horario = models.TimeField
     fecha = models.DateField
     consultorio = models.CharField
 
 class Doctores(models.Model):
-    nombre = models.CharField(MAX_LENGTH = 20)
-    apellido = models.CharField(MAX_LENGTH = 20)
-    dni = models.CharField(MAX_LENGTH = 8)
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    dni = models.CharField(max_length=8)
     especialidad = models.IntegerField
     consultorio = models.IntegerField
 
-class Pacientes():
-    nombre = models.CharField(MAX_LENGTH = 20)
-    apellido = models.CharField(MAX_LENGTH = 20)
-    dni = models.CharField(MAX_LENGTH = 8)
-    telefono = models.CharField(MAX_LENGTH = 20)
+class Pacientes(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    dni = models.CharField(max_length=8)
+    telefono = models.CharField(max_length=20)
     turno = models.BooleanField
     obra_social = models.IntegerField
+    
