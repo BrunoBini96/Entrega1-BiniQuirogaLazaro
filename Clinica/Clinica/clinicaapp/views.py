e1ec7b784e5806ff233c8e05ac6d525e7a56875e
Elizabeth Quiroga Castro <becquiroga@gmail.com>
2022-11-03 11:56:31 -0300

    creacion de views: list, detail, create, update y delete de doctores

--- Clinica/Clinica/clinicaapp/views.py
+++ Clinica/Clinica/clinicaapp/views.py
@@ -1,6 +1,42 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
+
+from .models import Doctores, Pacientes, Turnos
 
 def inicio(request):
     
     return render(request,"inicio.html")
 
+class DoctorList(ListView):
+
+    model = Doctores
+    template_name = 'doctores_list.html'
+    context_object_name = "doctor"
+
+class DoctorDetail(DetailView):
+
+    model = Doctores
+    template_name = 'doctores_detail.html'
+    context_object_name = "doctor"
+
+class DoctorCreate(CreateView):
+
+    model = Doctores
+    template_name = 'doctores_create.html'
+    fields = ["nombre", "apellido", "dni", "consultorio"]
+    success_url = ''
+
+class DoctorUpdate(UpdateView):
+
+    model = Doctores
+    template_name = 'doctores_update.html'
+    fields = ('__all__')
+    success_url = ''
+
+class DoctorDelete(DeleteView):
+
+    model = Doctores
+    template_name = 'doctores_delete.html'
+    success_url = ''
