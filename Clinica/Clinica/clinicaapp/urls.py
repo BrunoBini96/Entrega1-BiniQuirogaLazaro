29f30decfc2c2f24b4cb5bed6d59c2452f04278c
Elizabeth Quiroga Castro <becquiroga@gmail.com>
2022-11-03 12:19:03 -0300

    modificacion de doctores_list.html y url

--- Clinica/Clinica/clinicaapp/urls.py
+++ Clinica/Clinica/clinicaapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import inicio, formularioPaciente, pacientes, turnos, doctores, DoctorCreate, DoctorDetail
+from .views import inicio, formularioPaciente, pacientes, turnos, doctores, DoctorCreate, DoctorDetail, DoctorList
 
 urlpatterns = [
     path("",inicio),
     path("formularioPacientes/", formularioPaciente, name = 'PacientesFormulario'),
     path('pacientes/', pacientes , name = "Pacientes"),
-    path('doctores/', doctores, name = "Doctores"),
+    path('doctores/', DoctorList, name = "ListadoDoctores"),
     path('turnos/', turnos, name = "Turnos"),
     path('registrar-doctor/', DoctorCreate.as_view(), name="RegistrarDoctor"),
     path('detalle-doctor/', DoctorDetail.as_view(), name="DetalleDoctor"),
