346af4de5ac2a78cf40b1dca39cc9751eda453d6
Bruno <brunobini96@gmail.com>
2022-11-03 11:44:05 -0300

    Se agregan las redicciones a los botones de doctor,turnos y pacientes

--- Clinica/Clinica/clinicaapp/urls.py
+++ Clinica/Clinica/clinicaapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import inicio
+from .views import inicio, formularioPaciente, pacientes, turnos, doctores
 
 urlpatterns = [
     path("",inicio),
+    path("formularioPacientes/", formularioPaciente, name = 'PacientesFormulario'),
+    path('pacientes/', pacientes , name = "Pacientes"),
+    path('doctores/', doctores, name = "Doctores"),
+    path('turnos/', turnos, name = "Turnos"),
 ]
--- Clinica/Clinica/clinicaapp/views.py
+++ Clinica/Clinica/clinicaapp/views.py
@@ -1,6 +1,32 @@
+from .forms import PacienteFormulario
 from django.shortcuts import render
+from .models import Pacientes, Doctores, Turnos
 
 def inicio(request):
     
     return render(request,"inicio.html")
 
+def pacientes(request):
+    return render(request,'pacientes.html')
+
+def doctores(request):
+    return render(request,'doctores.html')
+
+def turnos(request):
+    return render(request, 'turnos.html')
+
+
+
+def formularioPaciente (request):
+    if request.method == 'POST':
+        
+        mi_formulario = PacienteFormulario(request.POST)
+        if mi_formulario.is_valid():
+            data = mi_formulario.cleaned_data
+            paciente = Pacientes(nombre=data['nombre'], apellido=data['apellido'], dni = data['dni'], telefono = data['telefono'], turno = data['turno'], obra_social = data['obra_social'])
+            paciente.save()
+    else:
+        mi_formulario = PacienteFormulario()
+    return render (request,'pacienteFormulario.html', {'mi_formulario': mi_formulario})  
+
+  
